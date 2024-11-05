c843c8cfe88c326f73d8bfa243bf480515defd50
GUSTAVOHOOO <167133343+GUSTAVOHOOO@users.noreply.github.com>
2024-11-05 20:19:13 -0300

    Add files via upload

---
+++ Televisor/Principal.java
@@ -0,0 +1,12 @@
+package Televisor;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Tela tela1 = new Tela(10, 9, false);
+		
+	}
+
+}
